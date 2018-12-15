16c1f40f33d09902e9fe86c51c92cf02d2375391
Gonzalo Aguirre <aguirre.gonzalo@gmail.com>
2018-12-14 22:49:52 -0300

    Animate text too

--- App.js
+++ App.js
@@ -4,26 +4,44 @@ import { StyleSheet, Text, View, Image, Animated } from 'react-native'
 export default class App extends React.Component {
   constructor() {
     super()
-    this.animation = new Animated.Value(0)
+    this.animationFirstStep = new Animated.Value(0)
+    this.animationSecondStep = new Animated.Value(0)
   }
 
   componentDidMount() {
-    Animated.spring(this.animation, {
-      toValue: 1,
-      duration: 1000,
-      useNativeDriver: true,
-    }).start()
+    Animated.parallel([
+      Animated.spring(this.animationFirstStep, {
+        toValue: 1,
+        useNativeDriver: true,
+      }),
+      Animated.timing(this.animationSecondStep, {
+        toValue: 1,
+        duration: 400,
+        delay: 200,
+        useNativeDriver: true,
+      }),
+    ]).start()
   }
 
   render() {
-    const imageTranslateX = this.animation.interpolate({ inputRange: [0, 1], outputRange: [400, 0] })
+    const imageTranslateX = this.animationFirstStep.interpolate({ inputRange: [0, 1], outputRange: [400, 0] })
     const imageStyle = {
       transform: [{ translateX: imageTranslateX }],
+    }
+
+    const textOpacity = this.animationSecondStep
+    const textScale = this.animationSecondStep.interpolate({ inputRange: [0, 1], outputRange: [0.5, 1] })
+    const textStyle = {
+      opacity: textOpacity,
+      transform: [{ scale: textScale }],
     }
 
     return (
       <View style={styles.container}>
         <Animated.Image source={require('./assets/logo-dev-day.png')} style={[styles.logo, imageStyle]} />
+        <Animated.View style={textStyle}>
+          <Text style={styles.text}>2018</Text>
+        </Animated.View>
       </View>
     )
   }
@@ -38,12 +56,13 @@ const styles = StyleSheet.create({
   },
 
   logo: {
-    transform: [{ scale: 0.8 }],
+    // transform: [{ scale: 0.8 }],
   },
 
   text: {
     color: '#242352',
     fontSize: 45,
     fontWeight: '400',
+    opacity: 1,
   },
 })
